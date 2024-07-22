6fe4cbd9f045e067218b5c3e4957f3844a066b04
Leandro Freire Cabral <88214669+lendrw@users.noreply.github.com>
2024-07-21 23:08:29 -0300

    css do contato

--- pro-jr/src/pages/Contact/Contact.jsx
+++ pro-jr/src/pages/Contact/Contact.jsx
@@ -1,13 +1,14 @@
-import './Contact.module.css'
+import styles from './Contact.module.css';
+import classNames from 'classnames';
 
 const Contact = () => {
   return (
-    <div className='view'>
-        <div>
+    <div className={classNames('view', styles.container)}>
+        <div className={classNames(styles.contato, styles.contatoContainer)}>
             <h2>Contato</h2>
 
             <h3>Fone</h3>
-            <p>(84) 99952-1516</p>
+            <p>(84) 99952-1516 - ProJr</p>
 
             <h3>Email</h3>
             <p>projrconsultoriaangicos@gmail.com</p>
@@ -15,13 +16,9 @@ const Contact = () => {
             <h3>Endereço</h3>
             <p>Rua: Gamaliel Martins Bezerra, 587, bairro Alto da Alegria – Angicos/RN.</p>
             <p>CEP: 59.515-000 UFERSA – Bloco de Professores II, sala 36</p>
-
-            <div>
-                icons
-            </div>
         </div>
-        <div>
-            localização
+        <div className={classNames(styles.map)}>
+            <p>localização</p>
         </div>
     </div>
   )
