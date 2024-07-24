3de657d44118708c8a4566774728034e0d5a960b
Davi Samuel <devdraggnerstudio@gmail.com>
2024-07-24 13:45:51 -0300

    Task 003 ServiceButton component content stylizations

--- pro-jr/src/components/ServicesButton/ServicesButton.jsx
+++ pro-jr/src/components/ServicesButton/ServicesButton.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
 import StarLogo from '../../assets/ProJr_ServicesStar.png'
+import style from '../ServicesButton/ServicesButton.module.css'
 
 const ServicesButton = ({ ServicesTitle, ServicesContent }) => {
-  const [ExpandServices, setExpandServices] = useState(false)
+  {/*const [ExpandServices, setExpandServices] = useState(false)*/}
   return (
-    <div>
-      <div>
+    <div id='containerServiceBtn' className={style.containerServiceBtn}>
+      <div className={style.ServiceBtnSecon}>
       <a href="#">
-        <img src={StarLogo} alt="Star 10" />  
+        <img className={style.ServiceBtnImg} src={StarLogo} alt="Star 10" />  
       </a>
-        <h3>{ ServicesTitle }</h3>
+        <h3 className={style.ServiceBtnTitle}>{ ServicesTitle }</h3>
       </div>
       <div>
-        <p>
+        <p className={style.ServiceButtonContent}>
           { ServicesContent }
         </p>
       </div>
