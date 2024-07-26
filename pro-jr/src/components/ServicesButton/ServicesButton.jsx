0cbf6f7c6c3c0aeda270aeb91c2e0f1de8736496
Davi Samuel <devdraggnerstudio@gmail.com>
2024-07-26 15:17:32 -0300

    Task 003 Component ServiceButton animation with hover behavior

--- pro-jr/src/components/ServicesButton/ServicesButton.jsx
+++ pro-jr/src/components/ServicesButton/ServicesButton.jsx
@@ -1,21 +1,32 @@
 import { useState } from 'react'
-import StarLogo from '../../assets/ProJr_ServicesStar.png'
-import style from '../ServicesButton/ServicesButton.module.css'
+import styles from '../ServicesButton/ServicesButton.module.css'
 
-const ServicesButton = ({ ServicesTitle, ServicesContent }) => {
-  {/*const [ExpandServices, setExpandServices] = useState(false)*/}
+const ServicesButton = ({ ServiceSrc, ServiceTitle, ServiceContent }) => {
+
+  const [isHovered, setIsHovered] = useState(false)
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+  
   return (
-    <div id='containerServiceBtn' className={style.containerServiceBtn}>
-      <div className={style.ServiceBtnSecon}>
-      <a href="#">
-        <img className={style.ServiceBtnImg} src={StarLogo} alt="Star 10" />  
-      </a>
-        <h3 className={style.ServiceBtnTitle}>{ ServicesTitle }</h3>
+    <div className={styles.serviceContainer}>
+
+      <div className={styles.serviceHeader}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+          <img className={`${styles.serviceImage} ${isHovered ? styles.serviceRotate : ''}`} src={ServiceSrc} alt="Star 10" />
+
+          <h3 className={styles.serviceTitle}>{ ServiceTitle }</h3>
       </div>
-      <div>
-        <p className={style.ServiceButtonContent}>
-          { ServicesContent }
-        </p>
+
+      <div className={`${styles.serviceContent} ${isHovered ? styles.serviceShow : ''}`}>
+        {ServiceContent}
       </div>
     </div>
   )
