4ab8ebd419019557fa9f3d5f9e54d2f2689b69de
Paulo Henrif <paulohenriquef931@gmail.com>
2024-07-29 16:53:17 -0300

    Dando inicio ao Impact na branch remota.

--- pro-jr/src/pages/Impact/Impact.jsx
+++ pro-jr/src/pages/Impact/Impact.jsx
@@ -1,23 +1,30 @@
-import './Impact.module.css'
+import React from 'react'
+import styles from '../Impact/Impact.module.css'
+import projrLogo from '../Impact/img/projrLogo.png'
 
 const Impact = () => {
-  return (
-    <div className='view'>
-        <h1>Impacto</h1>
-        <div>
-            <h2>+62</h2>
-            <h2>ESTUDANTES POTENCIALIZADOS</h2>
+    return (
+        <div className="view">
+        <section className={styles.section}>
+            <h1 className={styles.h1}>Impacto</h1>
+              <img src={projrLogo} className={styles.img} alt="" />
+                <div className={styles.impact}>
+                    <div className={styles.impactBox}>
+                        <h2 className={styles.h2}>+62</h2>
+                        <h3 className={styles.h3}>ESTUDANTES POTENCIALIZADOS</h3>
+                    </div>
+                    <div className={styles.impactBox}>
+                        <h2 className={styles.h2}>+39</h2>
+                        <h3 className={styles.h3}>PROJETOS ENTREGUES</h3>
+                    </div>
+                    <div className={styles.impactBox}>
+                        <h2 className={styles.h2}>+24</h2>
+                        <h3 className={styles.h3}>CLIENTES IMPACTADOS</h3>
+                    </div>
+                </div>
+        </section>
         </div>
-        <div>
-            <h2>+39</h2>
-            <h2>PROJETOS ENTREGUES</h2>
-        </div>
-        <div>
-            <h2>+24</h2>
-            <h2>CLIENTES IMPACTADOS</h2>
-        </div>
-    </div>
-  )
+    )
 }
-
+  
 export default Impact
