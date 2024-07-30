2d1d6f9fa0b31795562ca93fe4551c647e5dd964
Paulo Henrif <paulohenriquef931@gmail.com>
2024-07-30 10:52:57 -0300

    Finalizando o Impact.

--- pro-jr/src/pages/Impact/Impact.jsx
+++ pro-jr/src/pages/Impact/Impact.jsx
@@ -5,23 +5,25 @@ import projrLogo from '../Impact/img/projrLogo.png'
 const Impact = () => {
     return (
         <div className="view">
-        <section className={styles.section}>
-            <h1 className={styles.h1}>Impacto</h1>
-              <img src={projrLogo} className={styles.img} alt="" />
-                <div className={styles.impact}>
-                    <div className={styles.impactBox}>
-                        <h2 className={styles.h2}>+62</h2>
-                        <h3 className={styles.h3}>ESTUDANTES POTENCIALIZADOS</h3>
+        <section className={styles.section_up}>
+            <section className={styles.section}>
+                <img src={projrLogo} className={styles.img} alt="PROJRLOGO" />
+                <h1 className={styles.h1}>Impacto</h1>
+                    <div className={styles.impact}>
+                        <div className={styles.impactBox}>
+                            <h2 className={styles.h2}>+62</h2>
+                            <h3 className={styles.h3}>ESTUDANTES POTENCIALIZADOS</h3>
+                        </div>
+                        <div className={styles.impactBox}>
+                            <h2 className={styles.h2}>+39</h2>
+                            <h3 className={styles.h3}>PROJETOS ENTREGUES</h3>
+                        </div>
+                        <div className={styles.impactBox}>
+                            <h2 className={styles.h2}>+24</h2>
+                            <h3 className={styles.h3}>CLIENTES IMPACTADOS</h3>
+                        </div>
                     </div>
-                    <div className={styles.impactBox}>
-                        <h2 className={styles.h2}>+39</h2>
-                        <h3 className={styles.h3}>PROJETOS ENTREGUES</h3>
-                    </div>
-                    <div className={styles.impactBox}>
-                        <h2 className={styles.h2}>+24</h2>
-                        <h3 className={styles.h3}>CLIENTES IMPACTADOS</h3>
-                    </div>
-                </div>
+            </section>
         </section>
         </div>
     )
