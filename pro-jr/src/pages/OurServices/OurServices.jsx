e57a9cae191544ce48d4f1260d139c5a9e29956f
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2024-08-10 15:55:58 -0300

    .

--- pro-jr/src/pages/OurServices/OurServices.jsx
+++ pro-jr/src/pages/OurServices/OurServices.jsx
@@ -4,7 +4,147 @@ import StarLogo from '../../assets/ProJr_ServicesStar.png'
 const OurServices = () => {
   return (
     <div id="services" className={styles.services}>
-        
+        <h2>Nossos serviços</h2>
+        <div className={styles.container}>
+            <div>
+                <h2>MAPEAMENTO DE PROCESSOS</h2>
+                <p>
+                    O mapeamento de processos identifica
+                    minuciosamente todas as etapas, fluxos e
+                    objetivos de seus processos, mas também
+                    aponta com precisão onde estão as
+                    oportunidades de melhoria.
+                </p>
+            </div>
+            <div>
+                <h2>CRONOANÁLISE</h2>
+                <p>
+                    A cronoanálise é uma ferramenta de estudo
+                    de tempos e movimentos, e é a chave para a
+                    otimização em sua linha de produção ou
+                    logística! Com ela, você ganha controle
+                    absoluto sobre seu negócio e identifica
+                    oportunidades valiosas para maximizar a
+                    eficiência.
+                </p>
+            </div>
+            <div>
+                <h2>ELABORAÇÃO DE LAYOUT</h2>
+                <p>
+                    Processo de organização de espaço que leva
+                    em conta cada detalhe, desde equipamentos
+                    até o fluxo de produção. Criamos o
+                    posicionamento perfeito para seus
+                    equipamentos, garantindo o máximo
+                    aproveitamento e eficiência no fluxo de
+                    produção.
+                </p>
+            </div>
+            <div>
+                <h2>GESTÃO DE ESTOQUE</h2>
+                <p>
+                    Planejamento, execução e controle dos seus
+                    recursos da melhor forma. Garantindo que
+                    você compre a quantidade certa de matérias-
+                    primas, mantenha suas mercadorias
+                    organizadas e nunca enfrente falta ou
+                    excesso.
+                </p>
+            </div>
+            <div>
+                <h2>PREVISÃO DE DEMANDA</h2>
+                <p>
+                    Realização da estimativa com precisão da
+                    quantidade de produtos ou serviços que seus
+                    clientes necessitarão, permitindo que você
+                    organize suas operações de forma
+                    impecável. Evitando o excesso e a escassez,
+                    garantindo o fornecimento ideal.
+                </p>
+            </div>
+            <div>
+                <h2>ATENDIMENTO AO CLIENTE</h2>
+                <p>
+                    Conjunto de práticas e estratégias para
+                    melhor atender os seus clientes,
+                    identificando as necessidades, e assim
+                    garantir a sua fidelização para com a sua
+                    empresa.
+                </p>
+            </div>
+            <div>
+                <h2>MODELO DE NEGÓCIOS</h2>
+                <p>
+                    Técnicas de análise de mercado para criar,
+                    entregar e capturar valor de maneira
+                    impecável. É a partir do modelo de negócios
+                    que você pode criar a sua empresa.
+                </p>
+            </div>
+            <div>
+                <h2>PLANEJAMENTO ESTRATÉGICO</h2>
+                <p>
+                    Estabelecimento de metas, estratégias de
+                    ação precisas e identificação dos recursos
+                    necessários para alcançá-las. Essencial para
+                    orientar e revitalizar sua empresa em busca
+                    de resultados excepcionais.
+                </p>
+            </div>
+            <div>
+                <h2>IMPLEMENTAÇÃO E MELHORIA DE DELIVERY</h2>
+                <p>
+                    Gestão de pedidos, preparação e entrega de
+                    produtos ou serviços através de plataformas
+                    de delivery.
+                </p>
+            </div>
+            <div>
+                <h2>FLUXO DE CAIXA</h2>
+                <p>
+                    Mantenha total controle sobre suas finanças,
+                    registrando meticulosamente todas as
+                    entradas e saídas de dinheiro. Tenha uma
+                    visão nítida de sua situação financeira atual e
+                    futura, permitindo decisões informadas que
+                    impulsionam o crescimento e a estabilidade.
+                </p>
+            </div>
+            <div>
+                <h2>CUSTEIO E PRECIFICAÇÃO</h2>
+                <p>
+                    Identificação e analise a cerca do que está
+                    envolvido dentre os custos de produção e,
+                    em seguida, definição dos preços de venda,
+                    de forma estratégica, garantindo a melhor
+                    lucratividade para seu negócio.
+                </p>
+            </div>
+            <div>
+                <h2>VIABILIDADE FINANCEIRA</h2>
+                <p>
+                    Metodologia de análise de custo-benefício
+                    como o critério definitivo para determinar a
+                    viabilidade financeira de projetos ou
+                    negócios. Apurando meticulosamente os
+                    custos e benefícios, permitindo que você
+                    tome decisões informadas e seguras.
+                </p>
+            </div>
+            <div>
+                <h2>GESTÃO DE REDES SOCIAIS</h2>
+                <p>
+                    Oferecemos soluções personalizadas para
+                    impulsionar a presença digital da sua
+                    empresa, desde consultorias simples até
+                    gestão completa, incluindo estratégias de
+                    postagem, criação de conteúdo e design
+                    visual exclusivo para aumentar o
+                    engajamento e a relevância da sua marca
+                    online.
+                </p>
+            </div>
+        </div>
           
     </div>
   )
